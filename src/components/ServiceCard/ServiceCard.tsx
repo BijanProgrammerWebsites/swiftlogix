57cbc87aba9a2182db3d723a29af84b20e802b17
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-20 10:30:53 +0330

    [ADD] cards layout using flex

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -2,6 +2,22 @@ import { ReactElement } from "react";
 
 import styles from "./ServiceCard.module.css";
 
-export default function ServiceCard(): ReactElement {
-  return <div className={styles["service-card"]}></div>;
+type Props = {
+  icon: ReactElement;
+  title: string;
+  description: string;
+};
+
+export default function ServiceCard({
+  icon,
+  title,
+  description,
+}: Props): ReactElement {
+  return (
+    <div className={styles["service-card"]}>
+      <div className={styles.icon}>{icon}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>{description}</div>
+    </div>
+  );
 }
